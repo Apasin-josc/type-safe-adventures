87a7c1071ba0064f98d966e584e771bdaa20de87
José Omar <joseomar_coppola@hotmail.com>
2026-09-01 16:48:21 +0200

    adding to the project the bascis of typescript

--- ts-101/dist/index.d.ts
+++ ts-101/dist/index.d.ts
@@ -1,8 +1,2 @@
 export {};
-/**
- * functions
- */
-/**
- * return type inference
- */ 
 //# sourceMappingURL=index.d.ts.map
--- ts-101/dist/index.js
+++ ts-101/dist/index.js
@@ -49,7 +49,150 @@ const personAge = person.age;
 /**
  * functions
  */
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+//console.log(addTwoNumbers(30, 15))
+const substractTwoNumbers = (a, b) => {
+    return a - b;
+};
+//console.log(substractTwoNumbers(10, 5))
+function addAllNumbers(items) {
+    const total = items.reduce((a, c) => a + c, 0);
+    //console.log(total)
+}
+//console.log(addAllNumbers([5,7,9,11,3,2,1]))
 /**
  * return type inference
- */ 
+ * infering that result is of type string
+ */
+function formatGreeting(name, greeting) {
+    return `${greeting}, ${name}`;
+}
+const result = formatGreeting("mario", "hello");
+/* 🦔 any 🦔 */
+/**
+ * any type
+ */
+let anyAge;
+let title;
+anyAge = 30;
+anyAge = 'thirty';
+title = 25;
+title = {
+    hello: 'world'
+};
+/**
+ * any type in arrays
+ */
+let anyThings = ['hello', true, 30, null];
+anyThings.push(false);
+/**
+ * functions & any
+ */
+function addTogether(value) {
+    return value + value;
+}
+const resultOne = addTogether('hello');
+const resultTwo = addTogether(3);
+//useful when migrating from js to ts
+//because using any won't cause errors initially
+/* 🦔 tupples 🦔 */
+let personTup = ['mario', 30, true];
+/**
+ * tuples examples
+ */
+let hsla;
+hsla = [200, '100%', '100%', 1];
+let xy;
+xy = [94.7, 20.1];
+function useCoords() {
+    //get coords
+    const lat = 100;
+    const long = 50;
+    return [lat, long];
+}
+const [lat, long] = useCoords();
+/**
+ * named tuples
+ */
+let userTup;
+userTup = ['mario', 30];
+const authorOne = { name: 'mario', avatar: '/img/mario.png' };
+const authorTwo = { name: 'yoshi', avatar: '/img/yoshi.png' };
+const newPost = {
+    title: 'my first post',
+    body: 'something interesting',
+    tags: ['gaming', 'tech'],
+    created_at: new Date(),
+    author: authorOne
+};
+/**
+ * as function argument types
+ */
+function createPost(post) {
+    console.log(`Created post ${post.title} by ${post.author.name}`);
+}
+createPost(newPost);
+/**
+ * with arrays
+ */
+let posts = [];
+posts.push(newPost);
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return [r, g, b];
+}
+const colorOne = getRandomColor();
+const colorTwo = getRandomColor();
+console.log(colorOne);
+console.log(colorTwo);
+const userOne = { name: 'mario', score: 75 };
+function formatUser(user) {
+    console.log(`${user.name} has a score of ${user.score}`);
+}
+formatUser(userOne);
+/* 🦔 union types 🦔 */
+let someId;
+someId = 1;
+someId = '2';
+let email = null;
+email = "omarscoppola97@gmail.com";
+email = null;
+let anotherId;
+anotherId = 'asdasd123';
+anotherId = 5;
+/**
+ * union type pitfall
+ */
+function swapIdType(id) {
+    //can only use props and methods common to
+    //both number and string  types
+    //parseInt(id) --> not allowed
+    return id;
+}
+function swapIdTypeGuard(id) {
+    if (typeof id === 'string') {
+        //can use string methods and properties
+        return parseInt(id);
+    }
+    else {
+        //can use number methods and properties
+        return id.toString();
+    }
+}
+const idOne = swapIdTypeGuard(1);
+const idTwo = swapIdTypeGuard('2');
+console.log(idOne);
+console.log(idTwo);
+function logDetails(value) {
+    if (value.type === 'user') {
+        console.log(value.email, value.username);
+    }
+    if (value.type === 'person') {
+        console.log(value.firstname, value.age);
+    }
+}
 //# sourceMappingURL=index.js.map
